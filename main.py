94ba05aca6f556f6c7dbdd08a59d58b3823055f8
UtsavSharma2141 <utsav.sharma1211@gmail.com>
2022-07-04 18:51:02 -0400

    added some features

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import datetime
+import os
+
 import pyttsx3
 import speech_recognition as sr
+import wikipedia
+import webbrowser
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -20,26 +24,47 @@ def wishMe():
         speak("Good Afternoon sir")
     else:
         speak("Good Evening sir")
-    speak("hello my name is jarvis, how can i help you today?")
+    #speak("hello my name is jarvis, how can i help you today?")
 
 
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        #speak.say('Hey I am Listening..')
+        speak("how can i help you?")
         print("Working on your request")
-        #speak.runAndWait()
+        r.pause_threshold = 1;
         audio = r.listen(source)
     try:
         print("Recognizing")
         query = r.recognize_google(audio, language='en-in')
         print(f"User Said: {query}\n")
     except Exception as e:
-        print("Please Say it again..")
+        speak("Please Say it again..")
         return "None"
     return query
 
 
 if __name__ == '__main__':
-    wishMe()
-    takeCommand()
+    #wishMe()
+    while True:
+        query = takeCommand().lower()
+        # Executing different tasks
+        if 'wikipedia' in query:
+            speak("searching on wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+            webbrowser.get('chrome')
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+            webbrowser.get('chrome')
+
+        elif 'play music' in query:
+            webbrowser.open("https://music.youtube.com/")
+            webbrowser.get('chrome')
+
+
